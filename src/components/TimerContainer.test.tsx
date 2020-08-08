ac93a43c303169d1f9aa57970fb461d311d8181c
Translucent504 <fleshlime.1@gmail.com>
2020-08-08 13:39:37 +0500

    commit before updating packages with ncu

---
+++ src/components/TimerContainer.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import TimerContainer from './TimerContainer'
+
+describe('<TimerContainer />', () => {
+    test('should render digits', () => {
+        const { getByText } = render(<TimerContainer />)
+        const timeElement = getByText('00:00:00')
+        expect(timeElement).toBeInTheDocument()
+    })
+
+    test('should render Play and Reset buttons', () => {
+        const { getByText } = render(<TimerContainer />)
+        const playButton = getByText('Play')
+        const resetButton = getByText('Play')
+        expect(playButton).toBeInTheDocument()
+        expect(resetButton).toBeInTheDocument()
+    })
+
+    test('should render progress bar', () => {
+        const container = render(<TimerContainer />)
+        
+    })
+    
+    
+
+})
---
+++ src/components/TimerContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TimerContainer = () => {
+    return (
+        <div className='timerContainer'>
+            <div>
+                00:00:00
+            </div>
+            <div>
+                <span>Play</span>
+                <span>Reset</span>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TimerContainer
