2fad09d2766fde93d868f7904f95d56618ab395c
Translucent504 <fleshlime.1@gmail.com>
2020-08-10 11:48:50 +0500

    added playPause button component

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons'
+import './ToggleButton.css'
 
+type Props = {
+    toggleCounting: (e:any) => void;
+    state: boolean;
+}
 
-const ToggleButton:React.FC = () => {
-    const [currentState, setCurrentState] = useState('play')
+const ToggleButton: React.FC<Props> = ({toggleCounting, state}) => {
+
     return (
-        <>
-            <button className='play-pause'><i className="fas fa-play"/></button>
-        </>
+        <button data-testid='playPauseButton' className='toggle-button' onClick={toggleCounting}>
+            {!state
+                ? <FontAwesomeIcon icon={faPlayCircle} />
+                : <FontAwesomeIcon icon={faPauseCircle} />
+            }
+        </button>
     )
 }
 
